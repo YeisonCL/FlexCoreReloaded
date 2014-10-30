83bb5622fdfde23c57570d56be4b0fc15a96e55e
YeisonCL <yeisoncl94@gmail.com>
2014-10-30 09:22:15 -0600

    Listo los cambios de Arduino.

---
+++ FlexDesktop/ArduinoManager/ArduinoConnect.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexDesktop.ArduinoManager
+{
+    public static class ArduinoConnect
+    {
+        static string _puertoFisico = "COM6";
+        static int _baudRate = 38400;
+
+        public static void iniciarConexionRedInalambrica(string pSsid, string pPassword)
+        {
+            SerialPort puerto = new SerialPort(_puertoFisico, _baudRate);
+            puerto.Open();
+            puerto.Write("1&" + pSsid + "&" + pPassword + "&");
+            puerto.Close();
+        }
+
+        public static void setearParametrosConexionServer(string pIp, int pPuerto)
+        {
+            SerialPort puerto = new SerialPort(_puertoFisico, _baudRate);
+            puerto.Open();
+            puerto.Write("2&" + pIp + "&" + pPuerto.ToString() + "&");
+            puerto.Close();
+        }
+
+        public static void iniciarPago(string pCuentaDestino, decimal pMonto)
+        {
+            SerialPort puerto = new SerialPort(_puertoFisico, _baudRate);
+            puerto.Open();
+            puerto.Write("3&" + pCuentaDestino + "&" + pMonto.ToString() + "&");
+            puerto.Close();
+        }
+    }
+}
