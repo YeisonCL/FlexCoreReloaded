4f145aaf79cc5abf07f371799fd8be40fd3b8680
YeisonCL <yeisoncl94@gmail.com>
2014-11-01 22:20:24 -0600

    :D!!

--- FlexCoreRest/Controllers/PersonaFisicaController.cs
+++ FlexCoreRest/Controllers/PersonaFisicaController.cs
@@ -8,6 +8,8 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+using System.Windows.Forms;
+
 namespace FlexCoreRest.Controllers
 {
     public class PersonaFisicaController : ApiController
@@ -18,10 +20,15 @@ namespace FlexCoreRest.Controllers
         {
             try
             {
+                MessageBox.Show("Entre al post XD");
                 string _datosPost = Request.Content.ReadAsStringAsync().Result;
+                MessageBox.Show("Lei el post XD, HEX: " + _datosPost);
                 byte[] _physicalPersonByte = TransformingObjects.ConvertHexToBytes(_datosPost);
+                MessageBox.Show("Converti de bytes a hex");
                 PhysicalPersonDTO _physicalPerson = (PhysicalPersonDTO)TransformingObjects.ByteArrayToObject(_physicalPersonByte);
-                ClientsFacade.getInstance().newPhysicalPerson(_physicalPerson);
+                MessageBox.Show(_physicalPerson.getName());
+                ClientsFacade.getInstance().insertPhysicalPerson(_physicalPerson);
+                MessageBox.Show("Agregue el objeto a la base");
                 HttpResponseMessage _request = Request.CreateResponse(HttpStatusCode.OK, "True");
                 _request.Headers.Add("Access-Control-Allow-Origin", "*");
                 return _request;
@@ -47,6 +54,50 @@ namespace FlexCoreRest.Controllers
                 string _physicalPersonListHex = BitConverter.ToString(_physicalPersonListByte);
                 _physicalPersonListHex = _physicalPersonListHex.Replace("-", "");
                 HttpResponseMessage _request = Request.CreateResponse(HttpStatusCode.OK, _physicalPersonListHex);
+                _request.Headers.Add("Access-Control-Allow-Origin", "*");
+                return _request;
+            }
+            catch
+            {
+                HttpResponseMessage _request = Request.CreateResponse(HttpStatusCode.OK, "False");
+                _request.Headers.Add("Access-Control-Allow-Origin", "*");
+                return _request;
+            }
+        }
+
+        //PUT /persona/fisica?Anterior=valor&Nueva=valor
+        //Modifica una persona fisica
+        public HttpResponseMessage PutModificarPersonaFisica(string Anterior, string Nueva)
+        {
+            try
+            {
+                byte[] _physicalPersonDTOByteAnterior = TransformingObjects.ConvertHexToBytes(Anterior);
+                byte[] _physicalPersonDTOByteNueva = TransformingObjects.ConvertHexToBytes(Nueva);
+                PhysicalPersonDTO _physicalPersonDTOAnterior = (PhysicalPersonDTO)TransformingObjects.ByteArrayToObject(_physicalPersonDTOByteAnterior);
+                PhysicalPersonDTO _physicalPersonDTONueva = (PhysicalPersonDTO)TransformingObjects.ByteArrayToObject(_physicalPersonDTOByteNueva);
+                ClientsFacade.getInstance().updatePhysicalPerson(_physicalPersonDTONueva, _physicalPersonDTOAnterior);
+                HttpResponseMessage _request = Request.CreateResponse(HttpStatusCode.OK, "True");
+                _request.Headers.Add("Access-Control-Allow-Origin", "*");
+                return _request;
+            }
+            catch
+            {
+                HttpResponseMessage _request = Request.CreateResponse(HttpStatusCode.OK, "False");
+                _request.Headers.Add("Access-Control-Allow-Origin", "*");
+                return _request;
+            }
+        }
+
+        //DELETE /persona/fisica?Borrado=valor
+        //Borra una persona fisica de la base de datos
+        public HttpResponseMessage DeleteBorrarPersonaFisica(string Borrado)
+        {
+            try
+            {
+                byte[] _physicalPersonDTOByte = TransformingObjects.ConvertHexToBytes(Borrado);
+                PhysicalPersonDTO _physicalPersonDTO = (PhysicalPersonDTO)TransformingObjects.ByteArrayToObject(_physicalPersonDTOByte);
+                ClientsFacade.getInstance().deletePhysicalPerson(_physicalPersonDTO);
+                HttpResponseMessage _request = Request.CreateResponse(HttpStatusCode.OK, "True");
                 _request.Headers.Add("Access-Control-Allow-Origin", "*");
                 return _request;
             }
--- FlexCoreRest/Controllers/PersonaJuridicaController.cs
+++ FlexCoreRest/Controllers/PersonaJuridicaController.cs
@@ -33,5 +33,73 @@ namespace FlexCoreRest.Controllers
                 return _request;
             }
         }
+
+        //GET /persona/juridica?Busqueda=valor
+        //Obtener una persona juridica
+        public HttpResponseMessage GetObtenerPersonaJuridica(string Busqueda)
+        {
+            try
+            {
+                byte[] _juridicPersonDTOByte = TransformingObjects.ConvertHexToBytes(Busqueda);
+                PersonDTO _juridicPersonDTO = (PersonDTO)TransformingObjects.ByteArrayToObject(_juridicPersonDTOByte);
+                List<PersonDTO> _juridicPersonList = ClientsFacade.getInstance().searchJuridicalPerson(_juridicPersonDTO);
+                byte[] _juridicPersonListByte = TransformingObjects.ObjectToByteArray(_juridicPersonList);
+                string _juridicPersonListHex = BitConverter.ToString(_juridicPersonListByte);
+                _juridicPersonListHex = _juridicPersonListHex.Replace("-", "");
+                HttpResponseMessage _request = Request.CreateResponse(HttpStatusCode.OK, _juridicPersonListHex);
+                _request.Headers.Add("Access-Control-Allow-Origin", "*");
+                return _request;
+            }
+            catch
+            {
+                HttpResponseMessage _request = Request.CreateResponse(HttpStatusCode.OK, "False");
+                _request.Headers.Add("Access-Control-Allow-Origin", "*");
+                return _request;
+            }
+        }
+
+        //PUT /persona/juridica?Anterior=valor&Nueva=valor
+        //Modifica una persona juridica
+        public HttpResponseMessage PutModificarPersonaJuridica(string Anterior, string Nueva)
+        {
+            try
+            {
+                byte[] _juridicPersonDTOByteAnterior = TransformingObjects.ConvertHexToBytes(Anterior);
+                byte[] _juridicPersonDTOByteNueva = TransformingObjects.ConvertHexToBytes(Nueva);
+                PersonDTO _juridicPersonDTOAnterior = (PersonDTO)TransformingObjects.ByteArrayToObject(_juridicPersonDTOByteAnterior);
+                PersonDTO _juridicPersonDTONueva = (PersonDTO)TransformingObjects.ByteArrayToObject(_juridicPersonDTOByteNueva);
+                ClientsFacade.getInstance().updateJuridicalPerson(_juridicPersonDTONueva, _juridicPersonDTOAnterior);
+                HttpResponseMessage _request = Request.CreateResponse(HttpStatusCode.OK, "True");
+                _request.Headers.Add("Access-Control-Allow-Origin", "*");
+                return _request;
+            }
+            catch
+            {
+                HttpResponseMessage _request = Request.CreateResponse(HttpStatusCode.OK, "False");
+                _request.Headers.Add("Access-Control-Allow-Origin", "*");
+                return _request;
+            }
+        }
+
+        //DELETE /persona/juridica?Borrado=valor
+        //Borra una persona juridica de la base de datos
+        public HttpResponseMessage DeleteBorrarPersonaJuridica(string Borrado)
+        {
+            try
+            {
+                byte[] _juridicPersonDTOByte = TransformingObjects.ConvertHexToBytes(Borrado);
+                PersonDTO _juridicPersonDTO = (PersonDTO)TransformingObjects.ByteArrayToObject(_juridicPersonDTOByte);
+                ClientsFacade.getInstance().deleteJuridicalPerson(_juridicPersonDTO);
+                HttpResponseMessage _request = Request.CreateResponse(HttpStatusCode.OK, "True");
+                _request.Headers.Add("Access-Control-Allow-Origin", "*");
+                return _request;
+            }
+            catch
+            {
+                HttpResponseMessage _request = Request.CreateResponse(HttpStatusCode.OK, "False");
+                _request.Headers.Add("Access-Control-Allow-Origin", "*");
+                return _request;
+            }
+        }
     }
 }
