6a0a84319ebef73bcd3d2c8e9c8e29dcedd42f1d
jloaiza <joseph.lo.cr@gmail.com>
2014-11-07 00:55:13 -0600

    Se añade DTO para manejo de updates

---
+++ FlexCoreDTOs/general/UpdateDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexCoreDTOs.general
+{
+    class UpdateDTO<T>
+    {
+        public T _previous;
+        public T _new;
+
+        public UpdateDTO(T pPrevios, T pNew)
+        {
+            _previous = pPrevios;
+            _new = pNew;
+        }
+
+        public T getPrevious() { return _previous; }
+        public T getNew() { return _new; }
+    }
+}
