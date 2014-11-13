63cc2aa05def86de7e160c4120d4a8249e228c25
YeisonCL <yeisoncl94@gmail.com>
2014-11-13 01:45:38 -0600

    Terminado el arduino manager.

--- FlexDesktop/ArduinoManager/ArduinoConnect.cs
+++ FlexDesktop/ArduinoManager/ArduinoConnect.cs
@@ -3,37 +3,63 @@ using System.Collections.Generic;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace FlexDesktop.ArduinoManager
 {
     public static class ArduinoConnect
     {
         static string _puertoFisico = "COM6";
-        static int _baudRate = 38400;
+        static int _baudRate = 9600;
 
-        public static void iniciarConexionRedInalambrica(string pSsid, string pPassword)
+        public static string iniciarConexionRedInalambrica(string pSsid, string pPassword)
         {
+            string _respuesta = "";
             SerialPort puerto = new SerialPort(_puertoFisico, _baudRate);
             puerto.Open();
-            puerto.Write("1&" + pSsid + "&" + pPassword + "&");
+            puerto.WriteLine("1&" + pSsid + "&" + pPassword + "&");
+            while (_respuesta == "")
+            {
+                _respuesta = puerto.ReadExisting();
+                Thread.Sleep(1000);
+            }
+            puerto.Dispose();
             puerto.Close();
+            return _respuesta;
         }
 
-        public static void setearParametrosConexionServer(string pIp, int pPuerto)
+        public static string setearParametrosConexionServer(string pIp, int pPuerto)
         {
+            string _respuesta = "";
             SerialPort puerto = new SerialPort(_puertoFisico, _baudRate);
             puerto.Open();
-            puerto.Write("2&" + pIp + "&" + pPuerto.ToString() + "&");
+            puerto.WriteLine("2&" + pIp + "&" + pPuerto.ToString() + "&");
+            while (_respuesta == "")
+            {
+                _respuesta = puerto.ReadExisting();
+                Thread.Sleep(1000);
+            }
+            puerto.Dispose();
             puerto.Close();
+            return _respuesta;
         }
 
-        public static void iniciarPago(string pCuentaDestino, decimal pMonto)
+        public static string iniciarPago(string pCuentaDestino, decimal pMonto)
         {
+            string _respuesta = "";
             SerialPort puerto = new SerialPort(_puertoFisico, _baudRate);
             puerto.Open();
-            puerto.Write("3&" + pCuentaDestino + "&" + pMonto.ToString() + "&");
+            puerto.WriteLine("3&" + pCuentaDestino + "&" + pMonto.ToString() + "&");
+            while(_respuesta == "")
+            {
+                _respuesta = puerto.ReadExisting();
+                Thread.Sleep(1000);
+            }
+            puerto.Dispose();
             puerto.Close();
+            return _respuesta;
         }
     }
 }
