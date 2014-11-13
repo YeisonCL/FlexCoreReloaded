58e0e24f407ba931f2bcd01db6531abf85b8f6de
jloaiza <joseph.lo.cr@gmail.com>
2014-11-13 03:47:36 -0600

    se añade campo para fotos en generic

--- FlexCoreDTOs/clients/GenericPersonDTO.cs
+++ FlexCoreDTOs/clients/GenericPersonDTO.cs
@@ -14,16 +14,27 @@ namespace FlexCoreDTOs.clients
     {
         public GenericPersonDTO() { }
 
-        public GenericPersonDTO(int pPersonID, string pName, string pIDCard)
+        public byte[] _photoBytes;
+
+        //setters
+        public void setPhotoBytes(byte[] pFile) { _photoBytes = pFile; }
+
+        //getters
+        public byte[] getPhotoBytes() { return _photoBytes; }
+
+
+        public GenericPersonDTO(int pPersonID, string pName, string pIDCard, byte[] pPhoto = null)
             : base(pPersonID, pName, DTOConstants.DEFAULT_STRING, DTOConstants.DEFAULT_STRING, pIDCard)
         {
             setPersonType(PersonDTO.JURIDIC_PERSON);
+            _photoBytes = pPhoto;
         }
 
-        public GenericPersonDTO(int pPersonID, string pName, string pFirstLastName, string pSecondLastName, string pIDCard)
+        public GenericPersonDTO(int pPersonID, string pName, string pFirstLastName, string pSecondLastName, string pIDCard, byte[] pPhoto = null)
             : base (pPersonID, pName, pFirstLastName, pSecondLastName, pIDCard)
         {
             setPersonType(PersonDTO.PHYSICAL_PERSON);
+            _photoBytes = pPhoto;
         }
 
         public string getPersonType() { return _type; }
