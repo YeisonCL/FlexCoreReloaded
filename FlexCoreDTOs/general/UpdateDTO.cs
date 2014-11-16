7e7012648a8f6af789f5aea4dc5c960cbcd6f62d
jloaiza <joseph.lo.cr@gmail.com>
2014-11-16 03:33:18 -0600

    cambios para serializar updateDTO

--- FlexCoreDTOs/general/UpdateDTO.cs
+++ FlexCoreDTOs/general/UpdateDTO.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FlexCoreDTOs.general
 {
+    [Serializable]
+    [DataContract(Namespace = "")]
     public class UpdateDTO<T>
     {
         public T _previous;
         public T _new;
+
+        public UpdateDTO() { }
 
         public UpdateDTO(T pPrevios, T pNew)
         {
