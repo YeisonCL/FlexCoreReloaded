6aa16eb68d0362b8f4ac209b119dfc8b31979e5d
louislo0olz <louisdra8@outlook.com>
2014-11-16 20:42:37 -0600

    derp

---
+++ PruebasUnitarias/administration/HistoricoTransaccionalTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FlexCoreDAOs.administration;
+using FlexCoreDTOs.administration;
+
+namespace PruebasUnitarias.administration
+{
+
+    [TestFixture]
+    public class HistoricoTransaccionalTest
+    {
+        HistoricoTransaccionalQueriesDAO htq;
+
+        [TestFixtureSetUp]
+        public void setUp()
+        {
+            htq = new HistoricoTransaccionalQueriesDAO();
+        }
+
+        [Test]
+        public void insertHistoricoTransaccionalTest()
+        {
+            String pDescripcion = "Hola";
+            DateTime pFechaHoraEntrada = new DateTime(2014, 12, 12);
+            DateTime pFechaHoraSalida = new DateTime(2015, 12, 12);
+            string pEstado = "DERP";
+            int pVersionAplicacion = 1;
+            int idCuenta = 1;
+            int tipoTransaccion = 1;
+            try
+            {
+                htq.insertHistoricoTransaccional(pDescripcion, pFechaHoraEntrada, pFechaHoraSalida, pEstado, pVersionAplicacion,
+                idCuenta, tipoTransaccion);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void getHistoricoTransaccionalTest()
+        {
+            try
+            {
+                List<HistoricoTransaccionalDTO> l = htq.getHistoricoTransaccional();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void getHistoricoTransaccionalIdTransaccionTest()
+        {
+            try
+            {
+                List<HistoricoTransaccionalDTO> l = htq.getHistoricoTransaccional(1);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void getHistoricoTransaccionalDescripcionTest()
+        {
+            try
+            {
+                List<HistoricoTransaccionalDTO> l = htq.getHistoricoTransaccional("Hola");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void getIdHistoricoTransaccionalTest()
+        {
+            try
+            {
+                int l = htq.getIdHistoricoTransaccional("Hola");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void updateDescripcionTransaccionVueloTest()
+        {
+            try
+            {
+                htq.updateDescripcionHistoricoTransaccional(1, "Holis");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void deleteHistoricoTransaccionalTest()
+        {
+            try
+            {
+                htq.deleteHistoricoTransaccional(1);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
---
+++ PruebasUnitarias/administration/TipoTransaccionTest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FlexCoreDAOs.administration;
+using FlexCoreDTOs.administration;
+
+namespace PruebasUnitarias.administration
+{
+
+    [TestFixture]
+    public class TipoTransaccionTest
+    {
+        TipoTransaccionQueriesDAO tt;
+
+        [TestFixtureSetUp]
+        public void setUp()
+        {
+            tt = new TipoTransaccionQueriesDAO();
+        }
+
+        [Test]
+        public void insertHistoricoTransaccionalTest()
+        {
+            String pDescripcion = "Hola";
+            try
+            {
+                tt.insertTipoTransaccion(pDescripcion);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void getHistoricoTransaccionalTest()
+        {
+            try
+            {
+                List<TipoTransaccionDTO> l = tt.getTipoTransaccion();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void getHistoricoTransaccionalIdTransaccionTest()
+        {
+            try
+            {
+                List<TipoTransaccionDTO> l = tt.getTipoTransaccion(1);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void getTipoTransaccionDescripcionTest()
+        {
+            try
+            {
+                List<TipoTransaccionDTO> l = tt.getTipoTransaccion("Hola");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void getIdTipoTransaccionTest()
+        {
+            try
+            {
+                int l = tt.getIdTipoTransaccion("Hola");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void updateTipoTransaccionTest()
+        {
+            try
+            {
+                tt.updateTipoTransaccion(1, "Holis");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void deleteTipoTransaccionTest()
+        {
+            try
+            {
+                tt.deleteTipoTransaccion(1);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
