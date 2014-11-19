4a2ee92cc32fa730bb62e88bd0419c97b16c0bac
YeisonCL <yeisoncl94@gmail.com>
2014-11-19 03:40:11 -0600

    Arreglado bug en generador de cuentas

--- FlexCoreLogic/cuentas/Generales/GeneradorCuentas.cs
+++ FlexCoreLogic/cuentas/Generales/GeneradorCuentas.cs
@@ -7,51 +7,51 @@ namespace FlexCoreLogic.cuentas.Generales
 {
     internal static class GeneradorCuentas
     {
-        private static string generarCuentaAux()
+        private static string generarCuentaAux(SqlCommand pComando)
         {
             string _numeroCuenta = "";
+            string _numeroCuentaAux = "";
             int _semilla = (int)DateTime.Now.Millisecond;
             Random _random = new Random(_semilla);
-            for(int i = 0; i < 8; i++)
+            do
             {
-                int _numero = _random.Next(0, 10);
-                _numeroCuenta = _numeroCuenta + Convert.ToString(_numero);
-                System.Threading.Thread.Sleep(1);
-            }
-            string _numeroCuentaAux = new string(_numeroCuenta.ToCharArray().OrderBy(s => (_random.Next(2) % 2) == 0).ToArray());
+                for(int i = 0; i < 8; i++)
+                {
+                    int _numero = _random.Next(0, 10);
+                    _numeroCuenta = _numeroCuenta + Convert.ToString(_numero);
+                    System.Threading.Thread.Sleep(1);
+                }
+                _numeroCuentaAux = new string(_numeroCuenta.ToCharArray().OrderBy(s => (_random.Next(2) % 2) == 0).ToArray());
+            } while (CuentaAhorroDAO.existeCuenta(_numeroCuenta, pComando));
             return _numeroCuentaAux;
         }
 
         public static string generarCuenta(int pTipoCuenta, int pTipoMoneda, SqlCommand pComando)
         {
-            string _numeroCuenta = "";
-            do
+            string _numeroCuenta = generarCuentaAux(pComando);
+            if (pTipoCuenta == Constantes.AHORROVISTA)
             {
-                _numeroCuenta = generarCuentaAux();
-                if (pTipoCuenta == Constantes.AHORROVISTA)
+                if (pTipoMoneda == Constantes.COLONES)
                 {
-                    if (pTipoMoneda == Constantes.COLONES)
-                    {
-                        _numeroCuenta = "9" + _numeroCuenta;
-                    }
-                    else if (pTipoMoneda == Constantes.DOLARES)
-                    {
-                        _numeroCuenta = "8" + _numeroCuenta;
-                    }
+                    _numeroCuenta = "9" + _numeroCuenta;
+                }
+                else if (pTipoMoneda == Constantes.DOLARES)
+                {
+                    _numeroCuenta = "8" + _numeroCuenta;
+                }
 
-                }
-                else if (pTipoCuenta == Constantes.AHORROAUTOMATICO)
+            }
+            else if (pTipoCuenta == Constantes.AHORROAUTOMATICO)
+            {
+                if (pTipoMoneda == Constantes.COLONES)
                 {
-                    if (pTipoMoneda == Constantes.COLONES)
-                    {
-                        _numeroCuenta = "7" + _numeroCuenta;
-                    }
-                    else if (pTipoMoneda == Constantes.DOLARES)
-                    {
-                        _numeroCuenta = "6" + _numeroCuenta;
-                    }
+                    _numeroCuenta = "7" + _numeroCuenta;
                 }
-            } while (CuentaAhorroDAO.existeCuenta(_numeroCuenta, pComando));
+                else if (pTipoMoneda == Constantes.DOLARES)
+                {
+                    _numeroCuenta = "6" + _numeroCuenta;
+                }
+            }
             return _numeroCuenta;
         }
     }
