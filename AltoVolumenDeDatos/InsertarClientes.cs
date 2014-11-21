8f5cd4b8fa376b07ed2318f86edd2b2c5b91b1a2
YeisonCL <yeisoncl94@gmail.com>
2014-11-21 13:24:13 -0600

    posibilidad de cancelar insercion

--- AltoVolumenDeDatos/InsertarClientes.cs
+++ AltoVolumenDeDatos/InsertarClientes.cs
@@ -9,6 +9,7 @@ namespace AltoVolumenDeDatos
     public static class InsertarClientes
     {
         public static int _estadoInsercion = 0;
+        public static bool _banderaDeInsercion = true;
 
         public static void insertarPrimerCliente()
         {
@@ -41,29 +42,43 @@ namespace AltoVolumenDeDatos
         {
             foreach (DataRow row in pDataT.Rows)
             {
-                setEstadoInsercion(_estadoInsercion + 1);
-                PhysicalPersonDTO persona = new PhysicalPersonDTO();
-                string d = Convert.ToString(row.Field<string>(4));
-                PersonAddressDTO direccion = new PersonAddressDTO(d);
-                string t1 = Convert.ToString(row.Field<Double>(5));
-                string t2 = Convert.ToString(row.Field<Double>(6));
-                PersonPhoneDTO telefono1 = new PersonPhoneDTO(t1);
-                PersonPhoneDTO telefono2 = new PersonPhoneDTO(t2);
-                List<PersonAddressDTO> Dirs = new List<PersonAddressDTO>();
-                List<PersonPhoneDTO> Telefonos = new List<PersonPhoneDTO>();
-                Dirs.Add(direccion);
-                Telefonos.Add(telefono1);
-                Telefonos.Add(telefono2);
-                persona.setName(row.Field<string>(0));
-                persona.setPersonType(PersonDTO.PHYSICAL_PERSON);
-                string ced = Convert.ToString((int)row.Field<Double>(3));
-                persona.setIDCard(ced);
-                persona.setFirstLastName(row.Field<string>(1));
-                persona.setSecondLastName(row.Field<string>(2));
-                int idCliente = ClientsFacade.getInstance().newClientAndPerson(persona, Dirs, Telefonos, null, null);
-                InsertarCuentaAhorroVista.insertarCuentaAhorroVistaBase(idCliente);
-                InsertarCuentaAhorroAutomatico.insertarCuentaAhorroAutomaticoBase(idCliente, pNumeroCuentaDeduccion, pHoraInicio);
+                if(_banderaDeInsercion == true)
+                {
+                    setEstadoInsercion(_estadoInsercion + 1);
+                    PhysicalPersonDTO persona = new PhysicalPersonDTO();
+                    string d = Convert.ToString(row.Field<string>(4));
+                    PersonAddressDTO direccion = new PersonAddressDTO(d);
+                    string t1 = Convert.ToString(row.Field<Double>(5));
+                    string t2 = Convert.ToString(row.Field<Double>(6));
+                    PersonPhoneDTO telefono1 = new PersonPhoneDTO(t1);
+                    PersonPhoneDTO telefono2 = new PersonPhoneDTO(t2);
+                    List<PersonAddressDTO> Dirs = new List<PersonAddressDTO>();
+                    List<PersonPhoneDTO> Telefonos = new List<PersonPhoneDTO>();
+                    Dirs.Add(direccion);
+                    Telefonos.Add(telefono1);
+                    Telefonos.Add(telefono2);
+                    persona.setName(row.Field<string>(0));
+                    persona.setPersonType(PersonDTO.PHYSICAL_PERSON);
+                    string ced = Convert.ToString((int)row.Field<Double>(3));
+                    persona.setIDCard(ced);
+                    persona.setFirstLastName(row.Field<string>(1));
+                    persona.setSecondLastName(row.Field<string>(2));
+                    int idCliente = ClientsFacade.getInstance().newClientAndPerson(persona, Dirs, Telefonos, null, null);
+                    InsertarCuentaAhorroVista.insertarCuentaAhorroVistaBase(idCliente);
+                    InsertarCuentaAhorroAutomatico.insertarCuentaAhorroAutomaticoBase(idCliente, pNumeroCuentaDeduccion, pHoraInicio);
+
+                }
+                else 
+                {
+                    break;
+                }
             }
+            _banderaDeInsercion = true;
+        }
+
+        public static void cancelarInsercion()
+        {
+            _banderaDeInsercion = false;
         }
 
         public static void setEstadoInsercion(int pEstadoInsercion)
